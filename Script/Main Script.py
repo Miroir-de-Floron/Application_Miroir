4eda731ce90d2aa775fa9459a9da02e2a76f8e5d
Yanis-TRIGLIA <yanis.triglia@etu.univ-amu.fr>
2023-11-06 21:42:09 +0100

    Modification du script principal et refactoring des fichier du projet

--- Script/Main Script.py
+++ Script/Main Script.py
@@ -1,10 +1,11 @@
 import vlc
 import threading
 import time
-import sqlite3
 import pyttsx3
 import pygame
+import json
 
+############################################################################################### Déclaration
 # booléen pour savoir si une vidéo de prédiction est en cours de lecture ou non
 video_de_prediction = False
 
@@ -17,6 +18,9 @@ pygame.init()
 musique_de_fond = pygame.mixer.Sound("Ressource/Son/Musique_de_fond.mp3")
 voix_intro = pygame.mixer.Sound("Ressource/Son/voix_intro.mp3")
 
+fileObject = open("Data/data.json", "r")
+jsonContent = fileObject.read()
+obj_python = json.loads(jsonContent)
 ######################################################################################################
 
 
@@ -33,19 +37,26 @@ def lancement_voix_prediction(prediction):
     print("tts terminé")
 
 
+def lectureDepuisJsonAvecInput(nbTag, temps):
+    # On lance la vidéo de prédiction
+
+    for i in range(len(obj_python)):
+        if(obj_python[i]['tag'] == int(nbTag)):
+            tts = pyttsx3.init() 
+            tts.stop()
+            print("tts en cours ")
+            print(obj_python[i]['nom'])
+            tts.setProperty('rate', 135)
+            tts.setProperty('voice', 'fr+f5')
+            tts.say(obj_python[i]['prediction'][temps])
+            tts.runAndWait()
+            print("tts terminé")
+
 
 # Fonction pour la lecture des vidéos de prediction
 def lanceur_video_de_prediction():
-    #on se connecte à la base de données
-    baseDeDonnees = sqlite3.connect('instance/Tag.db')
-    #on crée un curseur pour pouvoir executer des requêtes
-    curseur1 = baseDeDonnees.cursor()
-    curseur2 = baseDeDonnees.cursor()
-    curseur1.execute("SELECT etat FROM tag WHERE nom = 120")
-    curseur2.execute("SELECT etat FROM tag WHERE nom = 123")
-    #on récupère les valeurs des tags
-    Tag1 = curseur1.fetchone()
-    Tag2 = curseur2.fetchone()
+  
+    ##### !!! Refaire Tag 
 
     #on récupère la valeur booléenne la video de prédiction
     global video_de_prediction  
@@ -58,54 +69,25 @@ def lanceur_video_de_prediction():
     media_prediction.get_media_player().set_fullscreen(True)
 
     while True:
-        # On met à jour les valeurs des tagsà chaque tour de boucle
-        curseur1.execute("SELECT etat FROM tag WHERE nom = 120")
-        curseur2.execute("SELECT etat FROM tag WHERE nom = 123")
-        Tag1 = curseur1.fetchone()
-        Tag2 = curseur2.fetchone()
 
-        # On vérifie si le tag 120 a été scanné
-        if Tag1[0] == True:
-            # On lance la vidéo de prédiction
+        input1 = input()
+        input2 = input()
+        input3 = input()
+
+        if(input3 != None):
             media_prediction.play()
             video_de_prediction = True
-            # On met à jour l'état du tag pour pouvoir le rescaner après
-            curseur1.execute("UPDATE tag SET etat = 0 WHERE nom = 120")
-            baseDeDonnees.commit()
-
-            # On lance le thread TTS
-            tts_thread = threading.Thread(target=lancement_voix_prediction, args=("Vous avez tirez la carte Lune, synonyme d'un grand avenir dans l'astronomie foudroyante.",))
-            tts_thread.start()
-            tts_thread.join()
-            
-            #une fois le thread TTS terminé, on arrête la vidéo de prédiction
+            lectureDepuisJsonAvecInput(input1, 'passe')
+            lectureDepuisJsonAvecInput(input2, 'present')
+            lectureDepuisJsonAvecInput(input3, 'futur')
             video_de_prediction = False
             media_prediction.stop()
 
-        # On vérifie si le tag 123 a été scanné
-        if Tag2[0] == True:
-            # On lance la vidéo de prédiction
-            media_prediction.play()
-            video_de_prediction = True
-             # On met à jour l'état du tag pour pouvoir le rescaner après
-            curseur2.execute("UPDATE tag SET etat = 0 WHERE nom = 123")
-            baseDeDonnees.commit()
-
-            # On lance le thread TTS
-            tts_thread = threading.Thread(target=lancement_voix_prediction, args=("Vous avez tiré la carte soleil, bohneur sur vous , plein de chance et de richesses",))
-            tts_thread.start()
-            tts_thread.join()
-
-            #une fois le thread TTS terminé, on arrête la vidéo de prédiction
-            video_de_prediction = False
-            media_prediction.stop()
-
-
+            fileObject.close()
 
 
 # Fonction pour la lecture de la vidéo d'introduction
 def lanceur_video_intro():
-
     #on configure la vidéo d'introduction
     default = lanceur.media_list_new()
     default.add_media(lanceur.media_new('Ressource/Video/Fond.mp4'))
@@ -141,6 +123,7 @@ def lanceur_video_intro():
 
         #on attend 0.2 secondes avant de relancer la boucle
         time.sleep(0.2)  
+
 
 # Création des threads et lancement des fonctions
 intro_thread = threading.Thread(target=lanceur_video_de_prediction)
